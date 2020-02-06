8069df69f16bcb2db5142256bc5b97efb0e83834
fancn <fanyj@yikeer.com>
2020-02-06 13:20:27 +0800

    form integration

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,29 @@
 import React from "react";
-import { DatePicker } from "antd";
+import { Tabs } from "antd";
 import "./App.css";
+import Uploader from "../Uploader/Uploader";
+import Unregister from "../Unregister/Unregister";
+
+const { TabPane } = Tabs;
+
+function callback(key) {
+  console.log(key);
+}
+
+const FACE_REGISTER = "人脸注册";
+const FACE_CANCEL_REGISTER = "人脸注册";
 
 function App() {
-  return <DatePicker />;
+  return (
+    <Tabs defaultActiveKey="1" onChange={callback}>
+      <TabPane tab={FACE_REGISTER} key="1">
+        <Unregister />
+      </TabPane>
+      <TabPane tab={FACE_CANCEL_REGISTER} key="2">
+        <Uploader />
+      </TabPane>
+    </Tabs>
+  );
 }
 
 export default App;
