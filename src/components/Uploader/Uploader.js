4d9034855ea00b55771522ca627302b475a915b8
fancn <fanyj@yikeer.com>
2020-02-07 12:03:42 +0800

    base64 from result

--- src/components/Uploader/Uploader.js
+++ src/components/Uploader/Uploader.js
@@ -3,23 +3,28 @@ import { Upload, Icon, message } from "antd";
 import "./Uploader.css";
 import { ADD_TASK } from "../../config/endpoints";
 
-function getBase64(img, callback) {
-  const reader = new FileReader();
-  reader.addEventListener("load", () => callback(reader.result));
-  reader.readAsDataURL(img);
-}
+// function getBase64(img, callback) {
+//   const reader = new FileReader();
+//   reader.addEventListener("load", () => {
+//     console.log(reader.result);
+//     callback(reader.result);
+//   });
+//   reader.readAsDataURL(img);
+// }
 
 function beforeUpload(file) {
-  const isJpgOrPng = file.type === "image/jpeg" || file.type === "image/png";
-  if (!isJpgOrPng) {
-    message.error("You can only upload JPG/PNG file!");
+  const isJpg = file.type === "image/jpeg";
+  if (!isJpg) {
+    message.error("请上传 JPG 格式的图片!");
   }
   const isLt2M = file.size / 1024 / 1024 < 2;
   if (!isLt2M) {
-    message.error("Image must smaller than 2MB!");
+    message.error("图片大小小于 2MB!");
   }
-  return isJpgOrPng && isLt2M;
+  return isJpg && isLt2M;
 }
+
+const codes = ["成功", "没有检测到人脸", "人脸分辨率过小", "上传的图片有误"];
 
 class Uploader extends React.Component {
   state = {
@@ -27,22 +32,37 @@ class Uploader extends React.Component {
   };
 
   handleChange = info => {
+    if (info.file.state === "error") {
+      message.error("图片上传失败!");
+      return;
+    }
     if (info.file.status === "uploading") {
       this.setState({ loading: true });
       return;
     }
-    debugger;
-    if (info.file.state === "error") {
-      message.error("Image failed to upload!");
-    }
     if (info.file.status === "done") {
-      // Get this url from response in real world.
-      getBase64(info.file.originFileObj, imageUrl =>
+      // 0 成功
+      // 1 没有检测到人脸
+      // 2 人脸分辨率过小
+      // 3 上传的图片有误
+      const {
+        response: { code, result }
+      } = info.file;
+      const resMsg = codes[code];
+      if (code === "0") {
+        message.success(resMsg);
         this.setState({
-          imageUrl,
+          imageUrl: `data:image/jpeg;base64,${result}`,
           loading: false
-        })
-      );
+        });
+        // Get this url from response in real world.
+        // getBase64(info.file.originFileObj, imageUrl =>
+        //   this.setState({
+        //     imageUrl,
+        //     loading: false
+        //   })
+        // );
+      } else message.error(resMsg);
     }
   };
 
