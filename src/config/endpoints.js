87635b650ff26cf59bf3b4902f572d3695960492
范轶洁 <fanyjb@digitalchina.com>
2020-02-08 17:29:47 +0800

    url formater

--- src/config/endpoints.js
+++ src/config/endpoints.js
@@ -1,3 +1,7 @@
-export const ADD_TASK = "http://10.3.69.41:8010/add_task";
-export const SIGNUP_TASK = "http://10.3.69.41:8010/signup_task";
-export const REMOVE_TASK = "http://10.3.69.41:8010/remove_task";
+const protocol = "http";
+const domain = "10.3.69.41";
+const port = ":8010";
+const urlPrefix = `${protocol}://${domain}${port}`;
+export const ADD_TASK = `${urlPrefix}/add_task`;
+export const SIGNUP_TASK = `${urlPrefix}/signup_task`;
+export const REMOVE_TASK = `${urlPrefix}/remove_task`;
